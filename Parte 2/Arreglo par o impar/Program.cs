877f966d3f635932033519a068ab4b74097d1918
theboxpe <perezregaladogabriel7@gmail.com>
2024-01-05 17:33:13 -0400

    Ok!

---
+++ Parte 2/Arreglo par o impar/Program.cs
@@ -0,0 +1,28 @@
+﻿/* PROGRAMA QUE LEA 10 NÚMEROS ENTEROS INGRESADOS POR EL USUARIO Y LOS INSERTE EN UN ARREGLO. A CONTINUACIÓN, 
+EL PROGRAMA DEBE CONTAR Y CLASIFICAR CUÁNTOS NÚMEROS PARES Y CUÁNTOS NÚMEROS IMPARES HAY DENTRO DEL ARREGLO, 
+Y MOSTRAR ESOS RESULTADOS.
+*/
+
+int a=0,b=0;
+
+int [] num;
+num = new int [10];
+for(int i=0;i<num.Length;i++)
+{
+    Console.WriteLine($"Digite un numero: {i+1} ");
+    num[i] = Convert.ToInt32(Console.ReadLine());
+
+    if(num[i]%2==0)
+    {
+        a++;
+    }
+    else
+    {
+        b++;
+    }
+    
+}
+Console.WriteLine($"Los numeros pares son: {a}");
+Console.WriteLine($"Los numeros pares son: {b}");
+
+Console.ReadKey();
---
+++ Parte 2/Arreglo simple/Program.cs
@@ -0,0 +1,21 @@
+﻿/* PROGRAMA QUE LE PIDA AL USUARIO 5 NÚMEROS Y LOS ALMACENE EN UN ARREGLO. 
+CUANDO EL USUARIO TERMINE DE INGRESARLOS  DEBE MOSTRAR LA SUMATORIA DE ESOS NÚMEROS.
+*/
+
+
+int a=0;
+int [] num;
+num = new int [4];
+
+for(int i=0;i<num.Length;i++)
+{
+    Console.WriteLine("Digite un numero: ");
+    num[i] = Convert.ToInt32(Console.ReadLine());
+    a+= num[i];
+
+}
+
+Console.WriteLine("La sumatoria de los digitos ingresados es: ");
+Console.WriteLine(a);
+
+Console.ReadKey();
